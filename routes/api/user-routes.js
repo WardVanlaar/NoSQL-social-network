f00c471aed1f6de8fbd6031886c8f1f59b041c79
Ward <wardv@tirf.ca>
2022-03-23 15:32:09 -0400

    trying to make friend routes work

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,6 +6,8 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  removeFriend,
 } = require("../../controllers/user-controller");
 
 // Set up GET all and POST at /api/user
@@ -13,5 +15,11 @@ router.route("/").get(getAllUser).post(createUser);
 
 // Set up GET one, PUT, and DELETE at /api/user/:id
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// Add a friend to user's friend list
+router.route('/:userId/friends/:friendId').post(addFriend);
+
+// Delete a friend from user's friend list
+router.route('/:userId/friends/:friendId').delete(removeFriend);
 
 module.exports = router;
